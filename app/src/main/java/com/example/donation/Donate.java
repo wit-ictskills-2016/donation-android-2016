ab37799f58f72aa58d7c1872c4ba6b88d384b7f8
John Fitzgerald <usplitu@hotmail>
2016-08-29 12:53:10 +0100

    Donation-01 step03 Donate button added

--- app/src/main/java/com/example/donation/Donate.java
+++ app/src/main/java/com/example/donation/Donate.java
@@ -1,13 +1,31 @@
 package com.example.donation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.widget.Button;
+import android.util.Log;
 
-public class Donate extends AppCompatActivity {
+public class Donate extends AppCompatActivity
+{
+
+    private Button donateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donate);
+
+        donateButton = (Button) findViewById(R.id.donateButton);
+        if (donateButton != null) {
+            Log.v("Donate", "Really got the donate button");
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_donate, menu);
+        return true;
     }
 }
