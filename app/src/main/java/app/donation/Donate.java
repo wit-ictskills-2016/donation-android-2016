bf0c6d33089c3d8ccf74f9a571533fe46c2c958e
John Fitzgerald <usplitu@hotmail>
2016-08-30 09:58:13 +0100

    Donation-03 step08 logout report & donate

--- app/src/main/java/app/donation/Donate.java
+++ app/src/main/java/app/donation/Donate.java
@@ -15,32 +15,31 @@ import android.widget.TextView;
 
 public class Donate extends AppCompatActivity
 {
-  private Button       donateButton;
-  private RadioGroup   paymentMethod;
-  private ProgressBar  progressBar;
+  private Button donateButton;
+  private RadioGroup paymentMethod;
+  private ProgressBar progressBar;
   private NumberPicker amountPicker;
-  private int          totalDonated;
-  private int          target;
+  private int totalDonated;
+  private int target;
 
-  private TextView     amountText;
-  private TextView     amountTotal;
+  private TextView amountText;
+  private TextView amountTotal;
 
   private DonationApp app;
 
   @Override
-  protected void onCreate(Bundle savedInstanceState)
-  {
+  protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_donate);
 
     app = (DonationApp) getApplication();
 
-    donateButton  = (Button) findViewById(R.id.donateButton);
-    paymentMethod = (RadioGroup)   findViewById(R.id.paymentMethod);
-    progressBar   = (ProgressBar)  findViewById(R.id.progressBar);
-    amountPicker  = (NumberPicker) findViewById(R.id.amountPicker);
-    amountTotal   = (TextView)     findViewById(R.id.amountTotal);
-    amountText    = (EditText)     findViewById(R.id.amountText);
+    donateButton = (Button) findViewById(R.id.donateButton);
+    paymentMethod = (RadioGroup) findViewById(R.id.paymentMethod);
+    progressBar = (ProgressBar) findViewById(R.id.progressBar);
+    amountPicker = (NumberPicker) findViewById(R.id.amountPicker);
+    amountTotal = (TextView) findViewById(R.id.amountTotal);
+    amountText = (EditText) findViewById(R.id.amountText);
 
     amountPicker.setMinValue(0);
     amountPicker.setMaxValue(1000);
@@ -52,26 +51,23 @@ public class Donate extends AppCompatActivity
   }
 
   @Override
-  public boolean onCreateOptionsMenu(Menu menu)
-  {
+  public boolean onCreateOptionsMenu(Menu menu) {
     // Inflate the menu; this adds items to the action bar if it is present.
     getMenuInflater().inflate(R.menu.menu_donate, menu);
     return true;
   }
 
 
-  public void donateButtonPressed (View view)
-  {
+  public void donateButtonPressed(View view) {
     String method = paymentMethod.getCheckedRadioButtonId() == R.id.PayPal ? "PayPal" : "Direct";
-    int donatedAmount =  amountPicker.getValue();
-    if (donatedAmount == 0)
-    {
+    int donatedAmount = amountPicker.getValue();
+    if (donatedAmount == 0) {
       String text = amountText.getText().toString();
-      if (!text.equals(""))
+      if (!text.equals("")) {
         donatedAmount = Integer.parseInt(text);
+      }
     }
-    if (donatedAmount > 0)
-    {
+    if (donatedAmount > 0) {
       app.newDonation(new Donation(donatedAmount, method));
       progressBar.setProgress(app.totalDonated);
       String totalDonatedStr = "$" + app.totalDonated;
@@ -84,11 +80,13 @@ public class Donate extends AppCompatActivity
   }
 
   @Override
-  public boolean onOptionsItemSelected(MenuItem item)
-  {
-    switch (item.getItemId())
-    {
-      case R.id.menuReport : startActivity (new Intent(this, Report.class));
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.menuReport:
+        startActivity(new Intent(this, Report.class));
+        break;
+      case R.id.menuLogout:
+        startActivity(new Intent(this, Welcome.class));
         break;
     }
     return true;
--- app/src/main/java/app/donation/Report.java
+++ app/src/main/java/app/donation/Report.java
@@ -19,35 +19,35 @@ import java.util.List;
 
 public class Report extends AppCompatActivity
 {
-  private ListView    listView;
+  private ListView listView;
   private DonationApp app;
 
   @Override
-  public void onCreate(Bundle savedInstanceState)
-  {
+  public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_report);
 
     app = (DonationApp) getApplication();
 
     listView = (ListView) findViewById(R.id.reportList);
-    DonationAdapter adapter = new DonationAdapter (this, app.donations);
+    DonationAdapter adapter = new DonationAdapter(this, app.donations);
     listView.setAdapter(adapter);
   }
 
   @Override
-  public boolean onCreateOptionsMenu(Menu menu)
-  {
+  public boolean onCreateOptionsMenu(Menu menu) {
     getMenuInflater().inflate(R.menu.menu_report, menu);
     return true;
   }
 
   @Override
-  public boolean onOptionsItemSelected(MenuItem item)
-  {
-    switch (item.getItemId())
-    {
-      case R.id.menuDonate : startActivity (new Intent(this, Donate.class));
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.menuDonate:
+        startActivity(new Intent(this, Donate.class));
+        break;
+      case R.id.menuLogout:
+        startActivity(new Intent(this, Welcome.class));
         break;
     }
     return true;
@@ -60,20 +60,18 @@ class DonationAdapter extends ArrayAdapter<Donation>
   private Context context;
   public List<Donation> donations;
 
-  public DonationAdapter(Context context, List<Donation> donations)
-  {
+  public DonationAdapter(Context context, List<Donation> donations) {
     super(context, R.layout.row_layout, donations);
-    this.context   = context;
+    this.context = context;
     this.donations = donations;
   }
 
   @Override
-  public View getView(int position, View convertView, ViewGroup parent)
-  {
+  public View getView(int position, View convertView, ViewGroup parent) {
     LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-    View view       = inflater.inflate(R.layout.row_layout, parent, false);
-    Donation donation   = donations.get(position);
+    View view = inflater.inflate(R.layout.row_layout, parent, false);
+    Donation donation = donations.get(position);
     TextView amountView = (TextView) view.findViewById(R.id.row_amount);
     TextView methodView = (TextView) view.findViewById(R.id.row_method);
 
@@ -84,8 +82,7 @@ class DonationAdapter extends ArrayAdapter<Donation>
   }
 
   @Override
-  public int getCount()
-  {
+  public int getCount() {
     return donations.size();
   }
 }
